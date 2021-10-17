160ce89b489514005967f191cce222d259371945
italo <you@example.com>
2021-10-17 10:42:08 -0300

    initial modal structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import logoImg from '../../assets/logo.svg';
-import { Container, Content } from './styles';
+import React, { useState } from "react";
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+import Modal from "react-modal";
 
 export function Header() {
-	return (
-		<Container>
-			<Content>
-				<img src={logoImg} alt="dt money" />
-				<button>Nova transação</button>
-			</Content>
-		</Container>
-	)
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpem] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpem(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpem(false);
+  }
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button onClick={handleOpenNewTransactionModal}>Nova transação</button>
+        <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar transação</h2>
+        </Modal>
+      </Content>
+    </Container>
+  );
 }
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,28 +1,28 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-	width: 100vw;
-	background: var(--blue);
-`
+  width: 100vw;
+  background: var(--blue);
+`;
 
 export const Content = styled.div`
-	margin: 0 auto;
-	max-width: 980px;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	padding: 1rem 1rem 10rem;
-	& button {
-		font-size: 1rem;
-		font-weight: bold;
-		color: #fff;
-		background: var(--blue-light);
-		border-color: transparent;
-		border-radius: 0.25rem;
-		padding: 0.5rem 1rem;
-		transition: filter 0.4s;
-	}
-	& button:hover{
-		filter: brightness(0.9);
-	}
-`
+  margin: 0 auto;
+  max-width: 980px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 1rem 10rem;
+  & button {
+    font-size: 1rem;
+    font-weight: bold;
+    color: #fff;
+    background: var(--blue-light);
+    border-color: transparent;
+    border-radius: 0.25rem;
+    padding: 0.5rem 1rem;
+    transition: filter 0.4s;
+  }
+  & button:hover {
+    filter: brightness(0.9);
+  }
+`;
