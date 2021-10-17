592fd83c0c98fd9c52ec8c375360a203dddfb403
italo <you@example.com>
2021-10-17 10:42:24 -0300

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
-	return (
-		<>
-			<Header />
-			<Dashboard />
-			<GlobalStyle />
-		</>
-	)
+  return (
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,21 @@ import { App } from "./App";
 import { createServer } from "miragejs";
 
 createServer({
-	routes() {
-		this.namespace = 'api';
-		this.get('/transactions', () => {
-			return [
-				{
-					id: 1,
-					title: 'Transaction 1',
-					amount: 400,
-					type: 'deposit',
-					categoty: 'food',
-					createdAt: new Date()
-				}
-			]
-		})
-	}
-})
+  routes() {
+    this.namespace = "api";
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          categoty: "food",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
